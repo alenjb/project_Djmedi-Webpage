62c5df276ff46d33cb5466a56e6072e0b2bb33db
alenjb <alenjb@naver.com>
2024-02-23 15:00:44 +0900

    fix(제약사소개 배경 이미지):제약사소개 배경 이미지 수정:
    - 제약사 소개 배경 이미지를 수정함

--- data/ebcontents/eb4_shop_018/ec_item_1708309712.php
+++ data/ebcontents/eb4_shop_018/ec_item_1708309712.php
@@ -1,5 +1,5 @@
 <?php
 if (!defined('_EYOOM_')) exit;
 $ec_item = array(
-	"0" => array("ci_no" => "112","ec_code" => "1708309712","ci_theme" => "eb4_shop_018","ci_state" => "1","ci_sort" => "1","ci_subject" => "a:3:{i:0;s:16:\"제약사 소개\";i:1;s:0:\"\";i:2;s:0:\"\";}","ci_text" => "a:2:{i:0;s:45:\"DJHERB와 함께하는 제약사들입니다.\";i:1;s:0:\"\";}","ci_content" => "","ci_link" => "a:2:{i:0;s:0:\"\";i:1;s:0:\"\";}","ci_target" => "a:2:{i:0;s:0:\"\";i:1;s:0:\"\";}","ci_img" => "a:1:{i:0;s:36:\"6ee1355202b4f61ac493c10e0300de38.jpg\";}","ci_period" => "1","ci_start" => "","ci_end" => "","ci_view_level" => "1","ci_regdt" => "2024-02-19 11:29:54"),
+	"0" => array("ci_no" => "112","ec_code" => "1708309712","ci_theme" => "eb4_shop_018","ci_state" => "1","ci_sort" => "1","ci_subject" => "a:3:{i:0;s:16:\"제약사 소개\";i:1;s:0:\"\";i:2;s:0:\"\";}","ci_text" => "a:2:{i:0;s:45:\"DJHERB와 함께하는 제약사들입니다.\";i:1;s:0:\"\";}","ci_content" => "","ci_link" => "a:2:{i:0;s:0:\"\";i:1;s:0:\"\";}","ci_target" => "a:2:{i:0;s:0:\"\";i:1;s:0:\"\";}","ci_img" => "a:1:{i:0;s:36:\"brand_background_image.png\";}","ci_period" => "1","ci_start" => "","ci_end" => "","ci_view_level" => "1","ci_regdt" => "2024-02-19 11:29:54"),
 );
